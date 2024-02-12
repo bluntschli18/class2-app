a9e3a167e9f63489d720eb6e224d7026114aa3c0
bluntschli18 <danielsmond443@gmail.com>
2024-02-12 15:21:58 +0100

    appUpdate

--- App.js
+++ App.js
@@ -1,27 +1,95 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View, Image, TouchableOpacity, Button } from "react-native";
 
-export default function App() {
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Welcome to my react native world</Text>
-      <StatusBar style="auto" />
+    <View
+      style={{
+        backgroundColor: "black",
+        height: `100%`,
+        alignItems: `center`,
+        justifyContent: `center`,
+      }}
+    >
+      {/* <Text style={{ color: "white", fontSize: 40, textAlign: `center`, justifyContent: `center` }}>DUOLINGO</Text> */}
+      <Image
+        source={require("./assets/Duolingo.png")}
+        style={{ width: 153, height: 170 }}
+      />
+
+      <Text
+        style={{
+          color: "white",
+          fontSize: 40,
+          fontWeight: `bold`,
+          color: `#2BD800`,
+          marginTop: 30,
+        }}
+      >
+        duolingo
+      </Text>
+
+      <Text
+        style={{
+          color: "white",
+          width: 255,
+          height: 44,
+          fontSize: 18,
+          textAlign: `center`,
+        }}
+      >
+        The free, fun, and effective way to learn a language!
+      </Text>
+
+      {/*  */}
+      <View style={{ marginTop: 120 }}>
+        {/* Button for GET STARTED */}
+        <TouchableOpacity
+          style={{
+            borderRadius: 16,
+            backgroundColor: `#2BD800`,
+            padding: 24,
+            width: 312,
+            margin: 10,
+          }}
+        >
+          <Text
+            style={{
+              color: `#191920`,
+              fontSize: 17,
+              fontWeight: 600,
+              textAlign: `center`,
+            }}
+          >
+            GET STARTED
+          </Text>
+        </TouchableOpacity>
+
+        {/* Button for I HAVE AN ACCOUNT */}
+        <TouchableOpacity
+          style={{
+            borderRadius: 16,
+            backgroundColor: `#191920`,
+            padding: 24,
+            width: 312,
+            marginTop: 20,
+            borderWidth: 2,
+            borderColor: `#525D70`,
+          }}
+        >
+          <Text
+            style={{
+              color: `white`,
+              fontSize: 15,
+              fontWeight: `bold`,
+              textAlign: `center`,
+            }}
+          >
+            I ALREADY HAVE AN ACCOUNT
+          </Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: 'blue',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-
-  text: {
-    color: 'white',
-    fontSize: 20,
-    fontWeight: 'bold',
-  }
-  
-});
+export default App;
